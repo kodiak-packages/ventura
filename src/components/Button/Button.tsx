6e8752b842d13e4c92836a6995515d0c27170df4
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-04-19 13:45:55 +0200

    Finish docz setup

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,35 @@
-import React, { SFC } from 'react';
+import React from 'react';
+
+import '../../index.css';
+import styles from './Button.module.css';
 
 export interface Props {
-  children: React.ReactChild;
+  children: string;
+  appearance: 'tactile' | 'flat';
+  kind: 'primary' | 'secondary';
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-export const Button: SFC<Props> = ({ children }: Props) => (
-  <button type="button">{children}</button>
-);
+export const Button: React.FC<Props> = ({
+  children,
+  appearance = 'tactile',
+  kind = 'kind',
+  onClick,
+}: Props) => {
+  const getAppearanceClass = (appearanceProp: typeof appearance) => {
+    if (appearanceProp === 'flat') {
+      return styles.appearanceFlat;
+    }
+    return styles.appearanceTactile;
+  };
+
+  const className: string[] = [styles.base];
+
+  className.push(getAppearanceClass(appearance));
+
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
