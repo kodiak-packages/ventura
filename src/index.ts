74ef01cd37c0a98be8d6a36cfd2f65a985b22ee3
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-05-01 15:52:53 +0200

    implement feather icons

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import 'modern-css-reset';
-
 import './index.css';
 
 export { default as Button } from './components/Button/Button';
-export { default as Icon } from './components/Icon/Icon';
+export * from 'react-feather';
