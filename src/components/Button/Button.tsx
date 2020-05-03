d31835b588383d92b8759615ea077fb045bc18cf
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-05-03 15:21:04 +0200

    create spinner component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import Spinner from '../utils/Spinner/Spinner';
+
 import styles from './Button.module.css';
 
 export interface Props {
@@ -9,18 +11,37 @@ export interface Props {
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   isDisabled: boolean;
   isLoading: boolean;
+  prefixIcon: React.Component;
+  suffixIcon: React.Component;
   name?: string;
 }
 
-const Button: React.FC<Props> = ({ children, type = 'primary', onClick, name }: Props) => {
+const Button: React.FC<Props> = ({
+  children,
+  type = 'primary',
+  onClick,
+  isDisabled,
+  isLoading,
+  prefixIcon,
+  suffixIcon,
+  name,
+}: Props) => {
   const buttonClassNames = classNames(styles.base, {
     [styles.typePrimary]: type === 'primary',
     [styles.typeSecondary]: type === 'secondary',
   });
 
   return (
-    <button className={buttonClassNames} type="button" onClick={onClick} data-testid={name}>
+    <button
+      disabled={isDisabled}
+      className={buttonClassNames}
+      type="button"
+      onClick={onClick}
+      data-testid={name}
+    >
+      {isLoading ? <Spinner className={styles.spinner} /> : prefixIcon}
       {children}
+      {isLoading || suffixIcon}
     </button>
   );
 };
