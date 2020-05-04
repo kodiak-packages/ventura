e06f7ead581b2a4319dfbfb2dffb7949b7b9ab4e
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-05-04 09:53:27 +0200

    fix(icons): control icon size through css

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export interface IconProps {
   color?: string;
-  size?: string | number;
+  className?: string;
 }
 
-const Icon: React.FC<IconProps> = ({ size = 24, color = 'currentColor' }: IconProps) => {
+const Icon: React.FC<IconProps> = ({ className, color = 'currentColor' }: IconProps) => {
   return (
     <>
-      {size}
       {color}
+      {className}
     </>
   );
 };
