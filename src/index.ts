effdfe738abc4d1261aeca7a3ad2e7560d9f8fd8
Robin Wijnant <robin@wijnant.me>
2020-06-01 17:16:39 +0200

    feat(formfield): add formfield component

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as Button } from './components/Button/Button';
 export { default as Input } from './components/Input/Input';
 export { default as Radio } from './components/Radio/Radio';
 export { default as Modal } from './components/Modal/Modal';
+export { default as FormField } from './components/FormField/FormField';
 export * from 'react-feather';
