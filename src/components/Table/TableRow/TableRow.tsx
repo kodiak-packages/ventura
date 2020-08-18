eb875e765d2e30cfc37956005787a9c92be60960
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-08-18 18:41:50 +0200

    fix(Table): hover state

--- src/components/Table/TableRow/TableRow.tsx
+++ src/components/Table/TableRow/TableRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import classNames from 'classnames';
 
 import styles from './TableRow.module.css';
@@ -6,12 +6,22 @@ import styles from './TableRow.module.css';
 type Props = {
   className?: string;
   children?: React.ReactNode;
+  onClick?: MouseEventHandler<HTMLTableRowElement>;
+  name?: string;
 };
 
-const TableRow: React.FC<Props> = ({ className, children }: Props) => {
-  const tableClassNames = classNames(styles.row, className);
+const TableRow: React.FC<Props> = ({ className, children, onClick, name }: Props) => {
+  const tableClassNames = classNames(
+    styles.row,
+    { [styles.hoverable]: Boolean(onClick) },
+    className,
+  );
 
-  return <tr className={tableClassNames}>{children}</tr>;
+  return (
+    <tr onClick={onClick} className={tableClassNames} data-testid={`row-${name}`}>
+      {children}
+    </tr>
+  );
 };
 
 export default TableRow;
