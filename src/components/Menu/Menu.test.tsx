23112bfac082004d9fc4b67a5d120b0a519b5927
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-10-04 17:25:06 +0200

    test(Menu): craete basic tests

---
+++ src/components/Menu/Menu.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import { GitHub } from '../../index';
+import Menu from './Menu';
+
+describe('TextArea', () => {
+  test('default snapshot', () => {
+    const component = (
+      <Menu>
+        <Menu.Item>Option 1</Menu.Item>
+        <Menu.Item prefixIcon={<GitHub />}>Option 2</Menu.Item>
+      </Menu>
+    );
+    const { asFragment } = render(component);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('default snapshot', () => {
+    const component = (
+      <Menu>
+        <Menu.Item>Option 1</Menu.Item>
+        <Menu.Item prefixIcon={<GitHub />}>Option 2</Menu.Item>
+      </Menu>
+    );
+    const { asFragment } = render(component);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('onClick should be triggered when clicked', () => {
+    const onClickFn = jest.fn();
+    const component = (
+      <Menu>
+        <Menu.Item name="click" onClick={onClickFn}>
+          Option 1
+        </Menu.Item>
+        <Menu.Item prefixIcon={<GitHub />}>Option 2</Menu.Item>
+      </Menu>
+    );
+    const { getByTestId } = render(component);
+
+    const menuItem = getByTestId('menu-item-click');
+    fireEvent.click(menuItem);
+
+    expect(onClickFn).toHaveBeenCalledTimes(1);
+  });
+});
