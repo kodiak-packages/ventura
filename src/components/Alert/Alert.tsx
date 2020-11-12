1c9ab9b101980146d50a83be4784d0a3c3c9ec98
Bram Vanhoutte <bram.vanhoutte.x@gmail.com>
2020-11-12 20:42:57 +0100

    feat(Alert): add succes intent

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AlertCircle } from 'react-feather';
+import { AlertCircle, CheckCircle } from 'react-feather';
 import classNames from 'classnames';
 
 import cssReset from '../../css-reset.module.css';
@@ -14,11 +14,22 @@ interface Props {
 }
 
 const Alert: React.FC<Props> = ({ intent = 'error', message, className }: Props) => {
-  const mergedClassNames = classNames(cssReset.ventura, styles.alert, className);
+  const mergedClassNames = classNames(
+    cssReset.ventura,
+    styles.alert,
+    {
+      [styles.alertError]: intent === 'error',
+      [styles.alertSuccess]: intent === 'success',
+    },
+    className,
+  );
 
   return (
     <div className={mergedClassNames}>
-      {intent === 'error' && <AlertCircle className={styles.icon} />}
+      {intent === 'error' && <AlertCircle className={classNames(styles.icon, styles.iconError)} />}
+      {intent === 'success' && (
+        <CheckCircle className={classNames(styles.icon, styles.iconSuccess)} />
+      )}
       <span className={styles.message}>{message}</span>
     </div>
   );
